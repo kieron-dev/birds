d828f9909efb3152a997f13834c8a9375b4918e2
Kieron Browne <kbrowne@pivotal.io>
2018-09-24 21:02:50 +0100

    Hello world web app

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	staticFileDir := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDir))
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	return r
+}
+
+func main() {
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
